317ac17d90e899c2b45b472df16b0a3f7f062dd8
VladilavDiD <vladdidyk14@gmail.com>
2024-10-28 12:23:08 +0200

    fix err

--- vue-lottery-app/src/helpers/auth-service.ts
+++ vue-lottery-app/src/helpers/auth-service.ts
@@ -1,40 +1,47 @@
 import axios from "axios";
-import type {AuthenticatedUser} from "@/models/authenticated-user";
-import {ref} from "vue";
+import type { AuthenticatedUser } from "@/models/authenticated-user";
+import { ref } from "vue";
 
 export interface AuthResponse {
     success: boolean;
     errorMessage: string;
 }
+
 export abstract class AuthService {
     public static userAuthenticated = ref(false);
     private static authenticatedUser: AuthenticatedUser | null = null;
+
     static async login(email: string, password: string): Promise<AuthResponse> {
         try {
-            const authResponse = await axios.post('auth/login',
-                {email, password});
-
-            const userData = await axios.get("auth/profile",{
+            const authResponse = await axios.post('auth/login', { email, password });
+            
+            const userData = await axios.get("auth/profile", {
                 headers: {
                     'Authorization': `Bearer ${authResponse.data.access_token}`
                 }
-            })
+            });
+
             this.userAuthenticated.value = true;
-            this.authenticatedUser = {...userData.data};
-            console.log("Authenticated user:");
-            console.log(this.authenticatedUser);
+            this.authenticatedUser = { ...userData.data };
+            console.log("Authenticated user:", this.authenticatedUser);
+
             return {
                 success: true,
                 errorMessage: ''
+            };
+        } catch (err) {
+            let errorMessage = 'An unknown error occurred';
+
+            if (err instanceof Error) {
+                errorMessage = err.message;
             }
-        } catch (err){
+
             return {
                 success: false,
-                errorMessage: err.message
+                errorMessage
             };
         }
     }
-
 
     static logout() {
         console.log(this.userAuthenticated.value);
